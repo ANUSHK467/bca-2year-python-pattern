beb163454d8ee527f28bd01dd73a705d66629788
ANUSHK467 <anushk.singh_bca24@gla.ac.in>
2025-09-02 23:03:46 +0530

    Create pattern 8.py

---
+++ pattern 8.py
@@ -0,0 +1,15 @@
+""" 
+question 8 - print pattern
+    *
+   **
+  ***
+ ****
+*****
+"""
+n=int(input("enter the number: "))
+for i in range(1,n+1):
+    for j in range(n+1-i):
+        print("_",end="")
+    for j in range(i):
+        print("*",end="")
+    print()

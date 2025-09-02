4e1917991369130ae6429db4cf2de96bfff5dbcf
ANUSHK467 <anushk.singh_bca24@gla.ac.in>
2025-09-02 23:10:09 +0530

    Create question 13.py

---
+++ question 13.py
@@ -0,0 +1,14 @@
+""" 
+question 13 - print like this
+1
+12
+123
+1234
+"""
+
+n=int(input("enter the number: "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+    

51e80f451374a0b07b6fc5bedf34492d221bc978
ANUSHK467 <anushk.singh_bca24@gla.ac.in>
2025-09-02 23:07:23 +0530

    Create question 11.py

---
+++ question 11.py
@@ -0,0 +1,18 @@
+""" 
+question 11 - print like this
+*         *
+**       **
+***     ***
+****   ****
+***********
+"""
+
+n=int(input("enter the number: "))
+for i in range(1,n+1):  
+    for j in range(i):
+        print("*",end="")
+    for j in range(2*n-2*i):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    print()

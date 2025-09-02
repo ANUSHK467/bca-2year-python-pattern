11a1284429b000275ab0b108f4dada2bcb4d285f
ANUSHK467 <anushk.singh_bca24@gla.ac.in>
2025-09-02 23:06:16 +0530

    Create pattern 10.py

---
+++ pattern 10.py
@@ -0,0 +1,17 @@
+"""
+question 10 - print pattern
+**********
+****  ****
+***    ***
+**      **
+*        *
+"""
+n=int(input("enter the number: "))
+for i in range(1,n+1):  
+    for j in range(n+1-i):
+        print("*",end="")
+    for j in range(2*(i-1)):
+        print(" ",end="")
+    for j in range(n+1-i):
+        print("*",end="")
+    print()
